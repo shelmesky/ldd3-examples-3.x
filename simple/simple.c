fe4c9c0dfc9a283fc1da6677d691c166735c1bbe
roy.lieu <roy.lieu@gmail.com>
2016-04-04 00:10:33 +0800

    simple: 注释内存映射函数

--- simple/simple.c
+++ simple/simple.c
@@ -81,6 +81,25 @@ static struct vm_operations_struct simple_remap_vm_ops = {
 	.close = simple_vma_close,
 };
 
+/*
+ * 第三个参数直接使用vma->vm_pgoff，
+ * 是因为这部分代码是直接从drivers/char/mem.c中拷贝而来，所以它模拟的设备工作方式也类似/dev/mem，
+ * 而/dev/mem的pgoff刚好等于pgn。
+ *
+ * 1.在进程的虚拟空间查找一块VMA.
+ * 2.将这块VMA进行映射.
+ * 3.如果设备驱动程序中定义了mmap函数,则调用它.
+ * 4.将这个VMA插入到进程的VMA链表中.
+ * 内存映射工作大部分由内核完成，驱动程序中的mmap函数只需要为该地址范围建立合适的页表，
+ * 并将vma->vm_ops替换为一系列的新操作就可以了。
+ *
+ * virt_addr代表要建立页表的用户虚拟地址的起始地址，remap_pfn_range函数为处于virt_addr和virt_addr+size之间的虚拟地址建立页表。
+ * pfn是与物理内存起始地址对应的页帧号，虚拟内存将要被映射到该物理内存上。
+ * 页帧号只是将物理地址右移PAGE_SHIFT位。在大多数情况下，VMA结构中的vm_pgoff赋值给pfn即可。
+ * remap_pfn_range函数建立页表，对应的物理地址是pfn<<PAGE_SHIFT到pfn<<(PAGE_SHIFT)+size。
+ * size代表虚拟内存区域大小。
+ */
+
 static int simple_remap_mmap(struct file *filp, struct vm_area_struct *vma)
 {
 	if (remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff,
@@ -98,12 +117,27 @@ static int simple_remap_mmap(struct file *filp, struct vm_area_struct *vma)
 /*
  * The fault version.
  */
+/*
+ * 每次只映射一个PAGE
+ * 1.找到缺页的虚拟地址所在的VMA。
+ * 2.如果必要，分配中间页目录表和页表。
+ * 3.如果页表项对应的物理页面不存在，则调用nopage函数，它返回物理页面的页描述符。
+ * 4.将物理页面的地址填充到页表中。
+ * 在上面第3步中，分配好的页目录和页表直接对应的是物理PAGE，
+ * 如果在不修改页表映射的情况下是这样，如果修改了就是别的PAGE物理地址。
+ * 下面的代码拿到的物理地址没有经过修改，所以是直接映射到物理内存的。
+ * 和上面的remap的行为一直，都是直接映射虚拟地址对应的物理地址。
+ * 和/dev/mem一致。
+ */
 int simple_vma_fault(struct vm_area_struct *vma,
                 struct vm_fault *vmf)
 {
 	struct page *pageptr;
+    // 得到起始物理地址保存在offset中
 	unsigned long offset = vma->vm_pgoff << PAGE_SHIFT;
+    // 得到vmf->virtual_address对应的物理地址，保存在physaddr中
 	unsigned long physaddr = (unsigned long)(vmf->virtual_address - vma->vm_start) + offset;
+    // 得到物理地址对应的页帧号，保存在pageframe中。
 	unsigned long pageframe = physaddr >> PAGE_SHIFT;
 
 // Eventually remove these printks
@@ -112,9 +146,11 @@ int simple_vma_fault(struct vm_area_struct *vma,
 	printk (KERN_NOTICE "Page at %p\n", virt_to_page (__va (physaddr)));
 	if (!pfn_valid(pageframe))
 		return VM_FAULT_SIGBUS;
+    // 由页帧号返回对应的page结构指针保存在pageptr中
 	pageptr = pfn_to_page(pageframe);
 	printk (KERN_NOTICE "page->index = %ld mapping %p\n", pageptr->index, pageptr->mapping);
 	printk (KERN_NOTICE "Page frame %ld\n", pageframe);
+    // 调用get_page增加pageptr指向页面的引用计数
 	get_page(pageptr);
         vmf->page = pageptr;
 	return 0;
